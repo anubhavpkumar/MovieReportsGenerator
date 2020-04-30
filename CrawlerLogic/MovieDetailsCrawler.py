b43be81003aba006c39e1af7353e503f3e1fd4ba
Anubhav Kumar <anu2anubhav@gmail.com>
2020-04-30 21:46:45 +0530

    Created Class (#1)
    Co-authored-by: Anubhav Kumar <anubhav.p.kumar@gmail.com>

---
+++ CrawlerLogic/MovieDetailsCrawler.py
@@ -0,0 +1,44 @@
+## Python File to Details from URL
+from RequestMethods import *
+from MySqlUtility import *
+from MovieMethods import *
+import threading
+
+class MovieDetailsCrawler:
+    MovieQueue = []
+    monthsArray = []
+    yearsArray = []
+    def __init__(self, monthsArray, yearsArray):
+        self.monthsArray = monthsArray
+        self.yearsArray = yearsArray
+
+    def PushRatingsToQueue(self, month, year):
+        MovieNameAndUrlList = GetMovieNameAndUrl(month, year)
+        for movieNameAndUrl in MovieNameAndUrlList:
+            movieData = MovieData.MovieData()
+            movieData.MovieName = movieNameAndUrl["Name"]
+            movieData.MovieUrl = movieNameAndUrl["Url"]
+            movieData.MovieMonth = month
+            movieData.MovieYear = year
+            BindRatingsAndCertificates(movieData)
+            self.MovieQueue.append(movieData)
+        print ("All pushed for month: " + month)
+
+    def InsertMovieIntoDatabaseFromQueue(self):
+        while(1):
+            if (len(self.MovieQueue) > 0):
+                movieData = self.MovieQueue.pop(0)
+                InsertMovieIntoDatabase(movieData, movieData.MovieMonth, movieData.MovieYear)
+
+    def StartCrawling(self):
+        threadList = []
+        print (str(self.monthsArray))
+        for year in self.yearsArray:
+            for month in self.monthsArray:
+                print ("Creating thread")
+                threadList.append(threading.Thread(target=self.PushRatingsToQueue, args=(month,year,)))
+        for tds in threadList:
+            print ("Starting thread")
+            tds.start()
+        y = threading.Thread(target=self.InsertMovieIntoDatabaseFromQueue)
+        y.start()
--- CrawlerLogic/MovieDetailsExtractor.py
+++ CrawlerLogic/MovieDetailsExtractor.py
@@ -1,50 +1,8 @@
 ## Python File to Details from URL
-from RequestMethods import *
-from MySqlUtility import *
-from MovieMethods import *
-import threading
-#from queue import Queue
+from MovieDetailsCrawler import *
 
+MonthsArray = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+YearsArray = ["2017"]
 
-
-def PushRatingsToQueue(months):
-    for year in YearList:
-        for month in months:
-            MovieNameAndUrlList = GetMovieNameAndUrl(month, year)
-            for movieNameAndUrl in MovieNameAndUrlList:
-                movieData = MovieData.MovieData()
-                movieData.MovieName = movieNameAndUrl["Name"]
-                movieData.MovieUrl = movieNameAndUrl["Url"]
-                movieData.MovieMonth = month
-                movieData.MovieYear = year
-                BindRatingsAndCertificates(movieData)
-                MovieQueue.append(movieData)
-            print ("All pushed for month: " + month)
-
-def InsertMovieIntoDatabaseFromQueue():
-    while(1):
-        if (len(MovieQueue) > 0):
-            movieData = MovieQueue.pop(0)
-            InsertMovieIntoDatabase(movieData, movieData.MovieMonth, movieData.MovieYear)
-
-
-
-
-global MovieQueue
-MovieQueue = []
-MonthList = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
-YearList = ["2018"]
-
-threadList = []
-for month in MonthList:
-    print ("Creating thread")
-    threadList.append(threading.Thread(target=PushRatingsToQueue, args=([month], )))
-
-for tds in threadList:
-    print ("Starting thread")
-    tds.start()
-
-y = threading.Thread(target=InsertMovieIntoDatabaseFromQueue)
-y.start()
-    
-            
+MDC = MovieDetailsCrawler(MonthsArray, YearsArray)
+MDC.StartCrawling()
